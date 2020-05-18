51c39537a955077c2db81ad29b46c12f9fb8bf15
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-05-18 23:33:35 +0530

    Stateless Functions Components

--- react-app/src/components/navbar.jsx
+++ react-app/src/components/navbar.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
 
-class Navbar extends Component {
-  // state = {  }
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <span className="navbar-brand mb-0 h1">
-          Navbar
-          <span className="badge badge-pill badge-primary">
-            {this.props.totalcounters}
-          </span>
-        </span>
-      </nav>
-    );
-  }
-}
+//Stateless functions components
 
-export default Navbar;
+const NavBar = (props) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <span className="navbar-brand mb-0 h1">
+        Navbar{"  "}
+        <span className="badge badge-pill badge-primary">
+          {props.totalcounters}
+        </span>
+      </span>
+    </nav>
+  );
+};
+
+export default NavBar;
