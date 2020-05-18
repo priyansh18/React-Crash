ae88d82c0fed3eb966b603c58d44b20cc44b9ce1
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-05-18 22:27:56 +0530

    Multiple Components in Sync

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/navbar";
+import Counters from "./components/counters";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <React.Fragment>
+      <Navbar />
+      <main className="container">
+        <Counters />
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ react-app/src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  // state = {  }
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <span className="navbar-brand mb-0 h1">Navbar</span>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
