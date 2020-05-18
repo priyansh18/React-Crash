ecea3d5d2fae7eb3a8a1e4c1c39c9ef0d42f8695
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-05-18 18:06:12 +0530

    Raise and Handling event

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -21,6 +21,12 @@ class Counter extends Component {
         >
           Increment
         </button>
+        <button
+          onClick={this.props.onDelete}
+          className="btn btn-info btn-sm m-2"
+        >
+          Delete
+        </button>
         {/* {this.state.tags.length === 0 && "Please create a new Tag"}
         <ul>{this.renderTags()}</ul> */}
       </div>
@@ -31,7 +37,7 @@ class Counter extends Component {
     console.log("Increment clicked!!", this);
     // this.state.value++; it won't work
     this.setState({
-      count: this.state.value + 1,
+      value: this.state.value + 1,
     });
   };
 
@@ -48,13 +54,13 @@ class Counter extends Component {
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count == 0 ? "warning" : "primary";
+    classes += this.state.value == 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count == 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value == 0 ? "Zero" : value;
   }
 }
 
--- react-app/src/components/counters.jsx
+++ react-app/src/components/counters.jsx
@@ -4,7 +4,7 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 4 },
+      { id: 1, value: 0 },
       { id: 2, value: 3 },
       { id: 3, value: 2 },
       { id: 4, value: 1 },
@@ -14,11 +14,19 @@ class Counters extends Component {
     return (
       <div>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} value={counter.value} />
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            value={counter.value}
+          />
         ))}
       </div>
     );
   }
+
+  handleDelete = () => {
+    console.log("called");
+  };
 }
 
 export default Counters;
