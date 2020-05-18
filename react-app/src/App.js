2051fe8a6821ba70892fd6d82ad73fdc1fa0416e
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-05-18 23:47:06 +0530

    no.of counters

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,10 +8,13 @@ import Counters from "./components/counters";
 class App extends Component {
   state = {
     counters: [
+      //source of truth--no. of ways
       { id: 1, value: 0 },
       { id: 2, value: 3 },
       { id: 3, value: 2 },
       { id: 4, value: 1 },
+      { id: 5, value: 1 },
+      { id: 6, value: 1 },
     ],
   };
   handleIncrement = (counter) => {
--- react-app/src/components/navbar.jsx
+++ react-app/src/components/navbar.jsx
@@ -6,7 +6,7 @@ const NavBar = ({ totalcounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <span className="navbar-brand mb-0 h1">
-        Navbar{"  "}
+        Items{"  "}
         <span className="badge badge-pill badge-primary">{totalcounters}</span>
       </span>
     </nav>
