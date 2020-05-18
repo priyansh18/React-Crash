67466e463bddea9c63a3bfd801cd59b2b32446d5
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-05-18 22:19:08 +0530

    Removing the local state

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -1,43 +1,43 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-    tags: ["tag1", "tag2", "tag3"],
-  };
+  // state = {
+  //   value: this.props.counter.value,
+  //   tags: ["tag1", "tag2", "tag3"],
+  // };
 
-  styles = {
-    fontSize: 15,
-    fontWeight: "bold",
-  };
+  // styles = {
+  //   fontSize: 15,
+  //   fontWeight: "bold",
+  // };
 
-  handleIncrement = () => {
-    // console.log("Increment clicked!!", this);
-    // this.state.value++; it won't work
-    this.setState({
-      value: this.state.value + 1,
-    });
-  };
+  // handleIncrement = () => {
+  //   // console.log("Increment clicked!!", this);
+  //   // this.state.value++; it won't work
+  //   this.setState({
+  //     value: this.state.value + 1,
+  //   });
+  // };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>"There are no tags"</p>;
+  // renderTags() {
+  //   if (this.state.tags.length === 0) return <p>"There are no tags"</p>;
 
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>tag</li>
-        ))}
-      </ul>
-    );
-  }
+  //   return (
+  //     <ul>
+  //       {this.state.tags.map((tag) => (
+  //         <li key={tag}>tag</li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value == 0 ? "warning" : "primary";
+    classes += this.props.counter.value == 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value == 0 ? "Zero" : value;
   }
 
@@ -48,7 +48,7 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button
-          onClick={() => this.handleIncrement()}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-danger btn-sm"
         >
           Increment
