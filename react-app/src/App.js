ae0bab692b6439155fb35ece293352ff9fc2265e
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-05-18 23:27:15 +0530

    Liftings the state up

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,18 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
+
 // import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/navbar";
 import Counters from "./components/counters";
 
-function App() {
-  return (
-    <React.Fragment>
-      <Navbar />
-      <main className="container">
-        <Counters />
-      </main>
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 3 },
+      { id: 3, value: 2 },
+      { id: 4, value: 1 },
+    ],
+  };
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    // console.log("called", counterId);
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar
+          totalcounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- react-app/src/components/navbar.jsx
+++ react-app/src/components/navbar.jsx
@@ -5,7 +5,12 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-light bg-light">
-        <span className="navbar-brand mb-0 h1">Navbar</span>
+        <span className="navbar-brand mb-0 h1">
+          Navbar
+          <span className="badge badge-pill badge-primary">
+            {this.props.totalcounters}
+          </span>
+        </span>
       </nav>
     );
   }
